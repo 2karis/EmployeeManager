28823cf1b8ef196657a1e6301816cf8edbb88a99
2karis <gitauk2@gmail.com>
2024-01-21 21:02:07 -0500

    added CORS rule

--- src/main/java/io/siliconsavannah/employeemanager/controller/EmployeeController.java
+++ src/main/java/io/siliconsavannah/employeemanager/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ={"http://localhost:4200", "http://someserver:80"})
 @RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
